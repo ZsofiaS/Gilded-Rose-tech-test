83fd83a67d4fb0f441cd5fd2c8c194034a641774
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-17 11:31:56 +0100

    Refactor: make the conditional in updateBackstagePass function more readable

--- src/gildedRoseShop.js
+++ src/gildedRoseShop.js
@@ -3,7 +3,6 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-
     this.items.forEach((item) => {
         this._updateByType(item)
       });
@@ -33,22 +32,16 @@ class Shop {
   }
 
   _updateBackstagePass(item) {
-    if (item.sellIn < 0) {
-       if (item.quality < 50) {
-         item.quality = 0;
-       }
-    } else if (item.sellIn <= 5) {
-       if (item.quality < 50) {
-         item.quality = item.quality + 3;
-       }
-    } else if (item.sellIn <= 10) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 2;
+    if (item.quality < 50) {
+      if (item.sellIn < 0) {
+           item.quality = 0;
+      } else if (item.sellIn <= 5) {
+           item.quality = item.quality + 3;
+      } else if (item.sellIn <= 10) {
+          item.quality = item.quality + 2;
+      } else if (item.sellIn > 10) {
+            item.quality++;
       }
-    } else if (item.sellIn > 10) {
-        if (item.quality < 50) {
-          item.quality++;
-        }
     }
     item.sellIn--;
   }
