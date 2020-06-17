b2e36ef927af7ec3aa4adf97841054691e0552b4
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-06-17 12:18:38 +0100

    Amend updateBackstagePass method so quality is 0 when sell by date passed

--- src/gildedRoseShop.js
+++ src/gildedRoseShop.js
@@ -30,7 +30,7 @@ class Shop {
   }
   _updateBackstagePass(item) {
     if (item.quality < 50) {
-      if (item.sellIn < 0) {
+      if (item.sellIn <= 0) {
            item.quality = 0;
       } else if (item.sellIn <= 5) {
            item.quality = item.quality + 3;
